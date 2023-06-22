c20939e00bc78fa83e19e95c01779333da43b5e2
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-22 11:17:44 +0300

    Решил задачу 2

---
+++ tasck_number_2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число:");
+int m = int.Parse(Console.ReadLine());
+if (m == 1)
+{
+    Console.WriteLine("Понедельник");
+} 
+else if (m == 2)
+{
+    Console.WriteLine("Вторник");
+} 
+else if (m == 3)
+{
+    Console.WriteLine("Среда");
+} 
+else if (m == 4)
+{
+    Console.WriteLine("Четверг");
+} 
+else if (m == 5)
+{
+    Console.WriteLine("Пятница");
+} 
+else if (m == 6)
+{
+    Console.WriteLine("Суббота");
+} 
+else if (m == 7)
+{
+    Console.WriteLine("Воскресенье");
+} 
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
