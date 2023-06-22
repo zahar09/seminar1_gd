e7edb6b95039789de3ec472a43c16bc611b211b9
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-22 11:38:00 +0300

    Поправил синтаксис в задаче 3

--- tasck_number_3/Program.cs
+++ tasck_number_3/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
-int i = number * -1;
+int i = -number;
 while (i < number+1)
 {
     Console.Write(i);
