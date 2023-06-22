e9ec5700c89fed49eac7a66bae2b0d66343cf52e
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-22 11:15:28 +0300

    Решил задачу 1

---
+++ tasck_number_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b * b){
+    Console.Write("Да");
+}
+else{
+    Console.Write("Нет");
+}
+
+
