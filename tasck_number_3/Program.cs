78ba0212dcd1cdf2840a6ca8b69fc302d56180b7
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-23 10:42:19 +0300

    Убрал унарный минус и добавил решение для ввода отрицательных чисел

--- tasck_number_3/Program.cs
+++ tasck_number_3/Program.cs
@@ -1,7 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
-int i = -number;
+int i;
+if(number >=0 ){
+    i = number * -1;
+}
+else{
+    i= number;
+    number = number * -1;
+}
+
+
 while (i < number+1)
 {
     Console.Write(i);
